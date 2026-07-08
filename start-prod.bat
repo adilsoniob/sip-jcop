90feecab869a05a93e513890b8a9ce5a283526aa
JCopSIP Deploy <deploy@jcopsip.app>
2026-07-07 22:46:12 -0300

    JCopSIP v2 - deploy 22:46

--- start-prod.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo ========================================
-echo  JCopSIP - Producao
-echo ========================================
-echo.
-set NODE_ENV=production
-cd backend
-node src/index.js
-pause
